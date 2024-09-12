4f05da537c942f1a52803dd4977f959f821010a1
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-09-12 14:09:19 -0400

    accout with methods

--- F24W2IntroToClasses/Program.cs
+++ F24W2IntroToClasses/Program.cs
@@ -6,6 +6,26 @@
         {
             Product p1 = new Product();
             Product p2 = new Product(101, "Laptop", 1200);
+
+
+            AccountWithMethods myAcc = new AccountWithMethods(100);
+            Console.WriteLine("\nBalance: " + myAcc.GetBalance().ToString("C"));
+
+            myAcc.Deposit(500);
+            Console.WriteLine("\nBalance: " + myAcc.GetBalance().ToString("C"));
+
+            try
+            {
+                myAcc.Withdraw(2000);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\n" + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("\nBalance: " + myAcc.GetBalance().ToString("C"));
+            }
         }
     }
 }

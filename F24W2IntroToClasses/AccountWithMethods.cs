ce0988639bff07fd9b104074fcd3b276a378a74d
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-09-12 14:01:47 -0400

    account with methods

---
+++ F24W2IntroToClasses/AccountWithMethods.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F24W2IntroToClasses
+{
+    public class AccountWithMethods
+    {
+        // fields
+        private double _balance;
+
+        // constructor
+        public AccountWithMethods(double intialBalance)
+        {
+            _balance = intialBalance;
+        }
+
+        // getters / setters
+        public double GetBalance()
+        {
+            return _balance;
+        }
+
+        public void SetBalance(double balance)
+        {
+            _balance = balance;
+        }
+
+        // methods
+        public void Deposit(double amount)
+        {
+            _balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > _balance)
+                throw new Exception("Amount cannot be more than the balance");
+            
+            _balance -= amount;
+        }
+    }
+}
